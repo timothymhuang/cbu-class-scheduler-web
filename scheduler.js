354971b37319725839f6a779d06638824f0d7b6c
Timothy <timophobic@gmail.com>
2023-12-19 17:11:22 -0800

    stuff

--- scheduler.js
+++ scheduler.js
@@ -149,6 +149,7 @@ function pageHome() {
         </div>
     </div>
     </div>`
+    console.log(readFile("/html/home.html"))
 }
 
 /************
@@ -1219,3 +1220,21 @@ function isWhitespaceOrEmpty(array) {
       return str.length === 0;
     });
   }
+
+
+
+function readSingleFile(evt) {
+    //Retrieve the first (and only!) File from the FileList object
+    var f = evt.target.files[0];
+
+    if (f) {
+        var r = new FileReader();
+        r.onload = function (e) {
+            var contents = e.target.result;
+            document.getElementById("ReadResult").innerHTML = contents;
+        }
+        r.readAsText(f);
+    } else {
+        alert("Failed to load file");
+    }
+}
